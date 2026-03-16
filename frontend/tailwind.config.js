63b641c60285f5816d5e9799ae5f16bcca1e696e
billu07 <billahmasum602@gmail.com>
2026-03-16 13:40:01 +0600

    there

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,6 +3,9 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['"Bai Jamjuree"', 'sans-serif'],
+      },
       colors: {
         gray: {
           750: "#2d3748",
